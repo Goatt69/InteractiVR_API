9b1b06f207ba18d39402785c365656b2d03aed48
Lil's G <quocduyq0@gmail.com>
2025-04-28 23:12:05 +0700

    Feat: Add middleware and adjust Objects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { SentryGlobalFilter, SentryModule } from '@sentry/nestjs/setup';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ObjectsModule } from './modules/objects/objects.module';
-import { SentryModule } from '@sentry/nestjs/setup';
-import { APP_FILTER } from '@nestjs/core';
-import { SentryGlobalFilter } from '@sentry/nestjs/setup';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './common/prisma/prisma.module';
+
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { AuthMiddleware } from './common/middlewares/auth.middleware';
 
 @Module({
   imports: [
@@ -26,6 +30,14 @@ import { PrismaModule } from './common/prisma/prisma.module';
       provide: APP_FILTER,
       useClass: SentryGlobalFilter,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AuthMiddleware,
+    },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware, AuthMiddleware).forRoutes('*');
+  }
+}
--- src/modules/objects/objects.controller.ts
+++ src/modules/objects/objects.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ObjectsService } from './objects.service';
 import { UpdateObjectDto } from './dto/update-object.dto';
-import { ApiResponse, ApiOperation } from '@nestjs/swagger';
-
+import {
+  ApiResponse,
+  ApiOperation,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserRole } from 'src/common/constants';
+import { Roles } from 'src/common/guards';
 @Controller('objects')
+@ApiTags('objects')
 export class ObjectsController {
   constructor(private readonly objectsService: ObjectsService) {}
 
@@ -15,16 +22,24 @@ export class ObjectsController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get an object by ID' })
+  @ApiOkResponse({ description: 'Object found successfully' })
   findOne(@Param('id') id: string) {
     return this.objectsService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Update an object by ID' })
+  @ApiOkResponse({ description: 'Object updated successfully' })
   update(@Param('id') id: string, @Body() updateObjectDto: UpdateObjectDto) {
     return this.objectsService.update(+id, updateObjectDto);
   }
 
   @Delete(':id')
+  @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Delete an object by ID' })
+  @ApiOkResponse({ description: 'Object deleted successfully' })
   remove(@Param('id') id: string) {
     return this.objectsService.remove(+id);
   }
