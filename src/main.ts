89fe763ec2b41d4a177ef65c2beb833dcce03381
Lil's G <quocduyq0@gmail.com>
2025-04-29 05:17:09 +0700

    feat: add BearerAuth authenticate to swagger

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     .setTitle('Interactive API')
     .setDescription('The Interactive API description')
     .setVersion('0.2')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
